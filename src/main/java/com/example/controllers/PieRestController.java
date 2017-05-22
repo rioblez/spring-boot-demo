28dc15f653c01c77ddb2008d8d43d178a36c6cf5
rioblez <rioblez@users.noreply.github.com>
2017-05-22 17:21:09 +0200

    v51

--- src/main/java/com/example/controllers/PieRestController.java
+++ src/main/java/com/example/controllers/PieRestController.java
@@ -65,6 +65,7 @@ public class PieRestController {
         @RequestMapping(method = RequestMethod.POST, value = "/toSofia2")
         public ResponseEntity<?> toSofia2(@RequestBody Object input) {
 
+			disable();
 			RestTemplate restTemplate = new RestTemplate();
 			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
@@ -108,4 +109,34 @@ public class PieRestController {
 			
         	return new ResponseEntity<JoinResponse>(join,HttpStatus.OK);
         }
+	
+	public static void disable() {
+        try {
+            SSLContext sslc = SSLContext.getInstance("TLS");
+            TrustManager[] trustManagerArray = { new NullX509TrustManager() };
+            sslc.init(null, trustManagerArray, null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new NullHostnameVerifier());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    private static class NullX509TrustManager implements X509TrustManager {
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            System.out.println();
+        }
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+            System.out.println();
+        }
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+    }
+ 
+    private static class NullHostnameVerifier implements HostnameVerifier {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    }
 }
