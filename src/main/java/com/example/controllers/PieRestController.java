556151443b82ea3560737ae18a75326383fea7ca
rioblez <rioblez@users.noreply.github.com>
2017-08-31 12:53:27 +0200

    v32

--- src/main/java/com/example/controllers/PieRestController.java
+++ src/main/java/com/example/controllers/PieRestController.java
@@ -75,8 +75,8 @@ public class PieRestController {
 			headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 			headers.set("X-SOFIA2-APIKey", "19ac492f7dad412faab7a1c8ef469b3b");
 			//HttpEntity<?> request = new HttpEntity<Object>("{\"demoTTNConnect\":"+input+"}", headers);
-			HttpEntity<?> request = new HttpEntity<Object>(""+input, headers);
-			//HttpEntity<?> request = new HttpEntity<Object>(input, headers);
+			//HttpEntity<?> request = new HttpEntity<Object>(""+input, headers);
+			HttpEntity<?> request = new HttpEntity<Object>(input, headers);
 			ResponseEntity<?> joinResponse = restTemplate.exchange(
 					"https://sofia2.com/sib-api/api/v2/demoTTN"
 					, HttpMethod.POST
